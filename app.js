e38dffd9cd0a287d6ba69b848b998ec3deda290f
Dosu333 <vladamir1865@gmail.com>
2023-09-20 12:37:02 +0100

    setup db

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express()
+
+// Environment variables
+require('dotenv/config');
+
+
+app.listen(3000, () => {
+    console.log('server is running on localhost');
+})
